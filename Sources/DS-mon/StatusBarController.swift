e22ccff14eef240341b1f292ce88283e8230cedd
Cherno76 <cherno@gmail.com>
2026-06-11 17:05:47 +0800

    fix: 总高保持16px，barGap调整为0.25

--- Sources/DS-mon/StatusBarController.swift
+++ Sources/DS-mon/StatusBarController.swift
@@ -268,7 +268,7 @@ class StatusBarView: NSView {
     // MARK: 布局常量
     private let barWidth: CGFloat = 5.0
     private let barHeight: CGFloat = 3.0
-    private let barGap: CGFloat = 1.0
+    private let barGap: CGFloat = 0.25
     private let columnGap: CGFloat = 1.0
     private let leadingGap: CGFloat = 1
 
